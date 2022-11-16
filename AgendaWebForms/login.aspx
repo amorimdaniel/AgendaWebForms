b283a80b705f3a8a982db6bcdc5bead3ac174697
Daniel Amorim <dan-amorim@live.com>
2022-11-16 09:38:33 -0300

    Criando tela de login

---
+++ AgendaWebForms/login.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="AgendaWebForms.login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="E-mail"></asp:Label>
+        </div>
+        <asp:TextBox ID="txbEmail" runat="server" Width="168px"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Senha"></asp:Label>
+        <br />
+        <asp:TextBox ID="txbSenha" runat="server"></asp:TextBox>
+        <p>
+            <asp:Button ID="btEntrar" runat="server" OnClick="Button1_Click" Text="Entrar" />
+        </p>
+        <p>
+    <asp:Label ID="msg" runat="server" Font-Bold="True"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ AgendaWebForms/login.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AgendaWebForms
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string email = txbEmail.Text;
+            string senha = txbSenha.Text;
+
+            System.Configuration.Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/MyWebSiteRoot");
+            System.Configuration.ConnectionStringSettings connString;
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["ConnectionString"];
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connString.ToString();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from usuario where email=@email and senha=@senha";
+            cmd.Parameters.AddWithValue("email", txbEmail.Text);
+            cmd.Parameters.AddWithValue("senha", txbSenha.Text);
+            con.Open();
+            SqlDataReader registro = cmd.ExecuteReader();
+            if (registro.HasRows)
+            {
+
+            }
+            else
+            {
+                msg.Text = "Email ou senha incorretos";
+            }
+
+           // cmd.ExecuteNonQuery();
+           // con.Close();
+        }
+    }
+}
